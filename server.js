b198fa8d6452526633000c55da2eee992781cb8b
Leigh Bitossi <leeroy77@gmailcom>
2021-02-13 17:40:32 +0900

    Changed Port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/addData', (req, res) => {
 
 // Setup Server
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const server = app.listen(port, ()=> {
     console.log(`Running on localhost: ${port}`)
